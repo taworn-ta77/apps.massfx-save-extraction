f42901c4cdce1626a7353e0cc8044f9791dc5c58
Taworn Ta. <taworn.ta7@gmail.com>
2026-02-05 11:24:34 +0700

    * extractor: add ExtractData1 (for ME1)

--- extractor/src/extract.rs
+++ extractor/src/extract.rs
@@ -1,2 +1,3 @@
+pub mod data1;
 pub mod save1;
 pub mod tools;
---
+++ extractor/src/extract/data1.rs
@@ -0,0 +1,6 @@
+#[derive(Default, Debug, Clone)]
+pub struct ExtractData1 {
+    pub url: String,
+    pub paragon: String,
+    pub name: String,
+}
